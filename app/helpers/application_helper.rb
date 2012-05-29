7a08e9a78b09c270b4fef430ded4e8457a5f352d
RohitRox <link2j.roy@gmail.com>
2012-05-30 00:29:37 +0545

    ui improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
    		end
 	end
 
+	def get_formatted_status(status)
+		case status
+		when "initial"
+			"&#8605;"
+		when "in-progress"
+			"&#8608;"
+		when "done"
+			"&#8607;"
+		when "dropped"
+			"&#8609;"
+		else
+			"Undefined"
+		end
+
+	end
+
 end
